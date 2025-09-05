2af5f52cbcc6b7ed8f01316943bf84a987ae49c4
SYNDESIS Plugin <syndesis.plugin@gmail.com>
2025-09-05 19:49:12 +0300

    Delete all files for migration

--- hrv_neural/mapping.py
+++
@@ -1,6 +0,0 @@
-HRV_FEATURES = ['meanRR','SDNN','RMSSD','LF','HF','LF_HF']
-CHAPTERS = ['Breathing','Stress','Recovery','Energy','Focus','Balance']
-
-def mapping_function():
-    """Return a toy mapping chapter -> HRV feature"""
-    return dict(zip(CHAPTERS, HRV_FEATURES))
